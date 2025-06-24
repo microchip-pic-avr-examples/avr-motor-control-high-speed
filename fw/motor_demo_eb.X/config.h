27287de1db1ad277180ee6510a968cfd95304e81
Teodor Lina <Teodor.Lina@microchip.com>
2025-06-24 17:43:23 +0300

    MPAE-19871: updated config.h comments

--- fw/motor_demo_eb.X/config.h
+++ fw/motor_demo_eb.X/config.h
@@ -25,34 +25,34 @@
 
 #define __BOARD_STRING__                "LowVoltage MPPB"
 #define __PLATFORM_STRING__             "AVR16EB32"
-#define DEBUG_BUFF_SIZE                 800  /* this has to be an even number */
+#define DEBUG_BUFF_SIZE                 800          /* This has to be an even number */
 
-/* analog thresholds */
-#define VIRTUAL_POTENTIOMETER           (0.00)       /* value: 0.1 ... 100.0 |  set to 0.00 for the real potentiometer usage */
-#define ADC_IIR_FILTER                  true         /* smoothing filter on analog adc functions: pot, vbus, current */
+/* Analog thresholds */
+#define VIRTUAL_POTENTIOMETER           (0.00)       /* Value: 0.1 ... 100.0 |  Set to 0.00 for the real potentiometer usage */
+#define ADC_IIR_FILTER                  true         /* Smoothing filter on ADC functions: POT, VBUS, current */
 
-/* board specific settings - MPPB */
-#define CURRENT_SHUNT_RESISTANCE        (0.01)       /* resistance in Ohm of the current shunt */
+/* Board specific settings - MPPB */
+#define CURRENT_SHUNT_RESISTANCE        (0.01)       /* Resistance in ohm of the current shunt */
 #define CURRENT_AMPLIFIER_GAIN          (7.5)
-#define CURRENT_AMPLIFIER_OFFSET        (1650.0)     /* mV - typical offset is Vref/2 = 1650mV  */
+#define CURRENT_AMPLIFIER_OFFSET        (1650.0)     /* mV - Typical offset is VREF/2 = 1650 mV  */
 #define VBUS_DIVIDER                    (16.0)
-#define ANALOG_REFERENCE                (3300.0)     /* millivolt */
-#define ADC_VBUS_TRIP                   (51000.0)    /* mV - vbus trip level */
-#define ADC_CURRENT_TRIP                (7000.0)     /* mA - adc current trip level */
-#define COMPARATOR_CURRENT_TRIP         (8000.0)     /* mA - comparator trip level - not used on external-comp configurations */
+#define ANALOG_REFERENCE                (3300.0)     /* mV */
+#define ADC_VBUS_TRIP                   (51000.0)    /* mV - VBUS trip level */
+#define ADC_CURRENT_TRIP                (7000.0)     /* mA - ADC current trip level */
+#define COMPARATOR_CURRENT_TRIP         (8000.0)     /* mA - Comparator trip level - not used on external-comparator configurations */
 #define FAULT_COMPARATOR_EN             false
 
-/* motor specific settings */
+/* Motor-specific settings */
 /* ACT42BLF01:  supply 24V, 3A limit */
-#define MOTOR_STARTUP_CURRENT           (2500.0)     /* milliamperes, initial alignment current pulse, may be higher than over-current protection */
-#define MOTOR_RPP                       (2.6)        /* ohms - motor resistance measured phase-to-phase */
-#define MOTOR_AMPRAMP_STEP_INTERVAL     (15)         /* amplitude ramp step interval: expressed milliseconds, time passed between two amplitude steps, 10 for aggresive ramp, 100 for soft ramp */
-#define MOTOR_ALIGNMENT_DURATION        (10)         /* milliseconds */
-#define MOTOR_MAXIMUM_ERPM              (50000.0)    /* e-rpm */
-#define STALL_THRS                      10           /* stall detection tolerance: higher number - more tolerant to perturbances. but slower detection */
+#define MOTOR_STARTUP_CURRENT           (2500.0)     /* mA - Initial alignment current pulse may be higher than overcurrent protection */
+#define MOTOR_RPP                       (2.6)        /* ohms - Motor resistance measured phase-to-phase */
+#define MOTOR_AMPRAMP_STEP_INTERVAL     (15)         /* Amplitude ramp step interval: expressed in milliseconds, time passed between two amplitude steps, 10 for aggresive ramp, 100 for soft ramp */
+#define MOTOR_ALIGNMENT_DURATION        (10)         /* ms */
+#define MOTOR_MAXIMUM_ERPM              (50000.0)    /* e-RPM */
+#define STALL_THRS                      10           /* Stall detection tolerance: higher number - more tolerant to perturbances, but slower detection */
 
-#define MOTOR_STARTUP_TIME              (1000)        /* milliseconds until POT is used | '-1' means potentiometer is ignored, both emulated and real */
-#define MOTOR_FORCED                    false         /* setting to 'true' makes the motor running with no BEMF sensing, only blind drive */
+#define MOTOR_STARTUP_TIME              (1000)       /* ms until POT is used | '-1' means potentiometer is ignored, whether it is real or emulated */
+#define MOTOR_FORCED                    false        /* Setting to 'true' makes the motor run with no BEMF sensing, only blind drive */
 
 #endif	/* CONFIG_H */
 
